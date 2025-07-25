28fa0b75be19a77f03df38592dc1aee545de3677
Patiño Angulo Alex <180604752+AlexP-Orion@users.noreply.github.com>
2025-07-24 23:36:52 -0500

    Probando cambios en login.js para hostear

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
+/*document.getElementById('loginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
 
   const cedula = document.getElementById('cedula').value;
@@ -18,5 +18,32 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     window.location.href = 'index.html';
   } else {
     alert(data.message);
+  }
+});
+*/
+document.getElementById('formLogin').addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const cedula = document.getElementById('cedulaLogin').value;
+  const password = document.getElementById('passwordLogin').value;
+
+  try {
+    const response = await fetch('https://citas-backend-5e65.onrender.com/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ cedula, password })
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      // Redirigir si el inicio es exitoso
+      window.location.href = 'calendario.html';
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    alert('Error en el servidor');
   }
 });
